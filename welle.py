40cd43882edf3e514824073036e3bf2b6f8e1ea4
Rocus van Oosten <rocus.van.oosten@gmail.com>
2025-08-04 13:32:57 +0200

    Update welle.py
    Line with EXTINF:-1,1 changed to EXTINF:-1,

--- welle.py
+++ welle.py
@@ -7,7 +7,7 @@ def gen_m3u ( station ):
     chn  = station [ 'channelName' ]
     print  ( name , hex(sid) , chn )
     with open ( name+'.m3u' , 'w' ) as p:
-        p.write ( '#EXTM3U\n#EXTINF:-1,1 '+name+'\nhttp://10.0.2.162:8888/mp3/'+hex(sid)+'/'+chn+'\n' )
+        p.write ( '#EXTM3U\n#EXTINF:-1,'+name+'\nhttp://10.0.2.162:8888/mp3/'+hex(sid)+'/'+chn+'\n' )
         p.close 
 
 with open('stations.json') as f:
